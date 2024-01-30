cd333a07d98b3d7d949e53fb92e4c1baef10f80b
Justin Condes <106140058+charlesjustin-condes@users.noreply.github.com>
2024-01-30 17:43:33 +0800

    first commit

---
+++ src/data/constants.js
@@ -0,0 +1,335 @@
+export const Bio = {
+    name: "Charles Justin Y. Condes",
+    roles: [
+      "QA Engineer",
+      "Full Stack Developer",
+      "UI/UX Designer",
+      "Project Manager",
+    ],
+    description:
+      "Hi! I am Justin, an ISTQB CTFL Certified Tester with more than a year of experience as a Quality Assurance Engineer.",
+    github: "",
+    resume:
+      "",
+    linkedin: "https://www.linkedin.com/in/charlesjustincondes/"
+  };
+  
+  export const skills = [
+    {
+      title: "Frontend",
+      skills: [
+        {
+          name: "React Js",
+          image:
+            "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+        },
+        {
+          name: "HTML",
+          image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+        },
+        {
+          name: "CSS",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+        },
+        {
+          name: "JavaScript",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+        },
+        {
+          name: "Bootstrap",
+          image:
+            "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+        }
+      ],
+    },
+    {
+      title: "Backend",
+      skills: [
+        {
+          name: "Node Js",
+          image: "https://nodejs.org/static/images/logo.svg",
+        },
+        {
+          name: "Python",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+        },
+        {
+          name: "MySQL",
+          image:
+            "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+        },
+      ],
+    },
+    {
+      title: "Android",
+      skills: [
+        {
+          name: "Android Studio",
+          image:
+            "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        },
+      ],
+    },
+    {
+      title: "Others",
+      skills: [
+        {
+          name: "Git",
+          image:
+            "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+        },
+        {
+          name: "GitHub",
+          image:
+            "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+        },
+        {
+          name: "VS Code",
+          image:
+            "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+        },
+        {
+          name: "Postman",
+          image:
+            "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+        },
+        {
+          name: "Figma",
+          image:
+            "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+        },
+      ],
+    },
+  ];
+  
+  export const experiences = [
+    {
+      id: 0,
+      img: "https://scontent.fmnl25-4.fna.fbcdn.net/v/t39.30808-6/273612196_1823704687817580_8195124889065640448_n.jpg?_nc_cat=109&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=e0mbbsi6trAAX8Ae_Th&_nc_ht=scontent.fmnl25-4.fna&oh=00_AfDYDbTsrP91Fs5fhNpEyPJ4x8jOtHQ9aMeT-rmUYGgEtQ&oe=65BD13C5",
+      role: "Quality Assurance Engineer",
+      company: "Yondu Inc.",
+      date: "July 2022 - Present",
+      desc: "Quality Assurance Engineer that focuses on guaranteeing the quality of the products and services. Responsible for Smoke Testing, Regression Testing, System Testing, API Testing, Functional Testing, Exploratory Testing, Codeless Automation Testing",
+      skills: [
+        "Creating Test Scenarios, Creating Test Cases, Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      doc: " ",
+    },
+    {
+      id: 1,
+      img: "https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/272584408_969125190360151_6447102037888681412_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=edWywwqp8OIAX8zxaq9&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfCXKSTfjOZaQhoxB0lBz8hyCSvFHrV_ZOh-iNgBI3iJOQ&oe=65BDD6ED",
+      role: "Quality Assurance Engineer Intern",
+      company: "Signet Properties",
+      date: "January 2022 - May 2022",
+      desc: "Responsible for Smoke Testing and Regression Testing of the company website, Test Scenario and Test Case Creation, Test Execution",
+      skills: [
+        "Creating Test Scenarios, Creating Test Cases, Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      doc: " ",
+    },
+    {
+      id: 2,
+      img: "https://scontent.fmnl25-1.fna.fbcdn.net/v/t39.30808-6/326911720_549479467110499_9081994011776899118_n.png?_nc_cat=105&ccb=1-7&_nc_sid=efb6e6&_nc_ohc=ltW4e5UDAQkAX-ACOx8&_nc_ht=scontent.fmnl25-1.fna&oh=00_AfC-UZkhb0JMWQSI9D-mr3oTZdDjpQ9TwXnjHXPptj6o5w&oe=65BD25B7",
+      role: "Production Group Trainee",
+      company: "Creotec Philippines, Inc.",
+      date: "December 2017 - December 2017",
+      desc: "Responsible in ensuring the quality of the device meets the standards of the company",
+      skills: [
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      doc: " ",
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: " ",
+      school: "University of Santo Tomas",
+      date: "August 2018 - May 2022",
+      grade: "1.93",
+      desc: "Consistent Dean's Lister",
+      degree: "Bachelor of Science in Information Technology major in Web and Mobile Application Development",
+    },
+    {
+      id: 1,
+      img: " ",
+      school: "University of Perpetual Help Rizal - Molino Campus",
+      date: "2016 - 2018",
+      degree: "Science, Technology, Engineering and Mathematics",
+    },
+    {
+      id: 2,
+      img: " ",
+      school: "Bacoor National Highschool - Molino Campus",
+      date: "2012 - 2016",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 11,
+      title: "Mobile Banking Application",
+      date: "June 2023 - Present",
+      description:
+        "Responsible for Internal Testing, Smoke Testing, Test Scenario Creation, Test Case Creation, Test Execution, Document Review",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/000080_Navy_Blue_Square.svg/900px-000080_Navy_Blue_Square.svg.png",
+      tags: [
+        "Creating Test Scenarios", "Creates & Executes Test Cases", "Exploratory Testing", "Defect Filing",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "android app"
+    },
+    {
+      id: 2,
+      title: "Japanese Finance Company focuses on Car Loans",
+      date: "November 2023 - December 2023",
+      description:
+        "Responsible for Regression Testing",
+      image:
+        "https://htmlcolorcodes.com/assets/images/colors/dark-green-color-solid-background-1920x1080.png",
+      tags: [
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "web app",
+    },
+    {
+      id: 9,
+      title: "Courier Service Application",
+      date: "April 2023 - May 2023",
+      description:
+        "Responsible for Test Scenario Creation, Test Case Creation, Test Execution, Explotory Testing, Defect Filing",
+      image:
+        "https://www.researchgate.net/profile/Vladimir-Petrusevski/publication/215904051/figure/fig3/AS:669509312577542@1536634867235/A-simulated-pair-of-complementary-blue-orange-colours.jpg",
+      tags: [
+        "Creating Test Scenarios", "Creating Test Cases", "Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "android app"
+    },
+    {
+      id: 0,
+      title: "Bookstore CMS",
+      date: "January 2023 - March 2023",
+      description:
+        "Responsible for Codeless Automation Testing, Test Scenario Creation, Test Case Creation, Test Execution, Explotory Testing, Defect Filing",
+      image:
+        "https://d1ka0itfguscri.cloudfront.net/zyIy/2024/01/30/08/00/cZVvjcVJNWz/preview.jpg",
+      tags: [
+        "Codeless Automation", "Creating Test Scenarios", "Creating Test Cases", "Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "android app"
+    },
+    {
+      id: 1,
+      title: "Fast-food Delivery Application",
+      date: "November 2022 - December 2022",
+      description:
+        "Responsible for Codeless Automation Testing, Test Scenario Creation, Test Case Creation, Test Execution, Explotory Testing, Defect Filing",
+      image:
+        "https://www.schemecolor.com/wallpaper?i=36081&desktop",
+      tags: [
+        "Codeless Automation", "Creating Test Scenarios", "Creating Test Cases", "Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "web app"
+    },
+    {
+      id: 2,
+      title: "Mobile Banking Application",
+      date: "September 2022 - November 2022",
+      description:
+        "Responsible for Smoke Testing, Regression Testing, Defect Filing, Exploratory Testing",
+      image:
+        "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/000080_Navy_Blue_Square.svg/900px-000080_Navy_Blue_Square.svg.png",
+      tags: [
+        "Creating Test Scenarios", "Creating Test Cases", "Executing Test Cases",
+        "Critical Thinking",
+        "Attention to Detail",
+        "Ability to work as part of a team",
+        "Knowledge of quality standards",
+        "Problem Solving",
+        "Multitasking",
+      ],
+      category: "android app"
+    },
+    {
+      id: 3,
+      title: "Capstone Project Directory System for UST ITDepartment",
+      date: "2020-2021",
+      description:
+        "Responsible in Project Management, Ensures that the team delivers the product within time frame and quality of the product is inline with the clients standard, Plans and develop the project ideas, Monitors the progress of the project, Ensures Stakeholders satisfaction, Coordinates with the stakeholders, Manages documents and reports, Directs the team, Develop Front End and Back End of the project",
+      image:
+        "https://d1ka0itfguscri.cloudfront.net/zyIy/2024/01/30/07/01/cZVviVVJNKl/preview.jpg",
+      tags: [
+        "Project Management", "UI/UX Designer", "Technical Documentation", "Leading the Team", "Agile Methodology", "Trello", "Github", "PHP", "MYSQL"
+      ],
+      category: "web app"
+    },
+    {
+      id: 10,
+      title: "CMS and eCommerce site for GoodRides by GoodGuys",
+      date: "2019 - 2020",
+      description:
+        "Responsible in Project Management, Ensures that the team delivers the product within time frame and quality of the product is inline with the clients standard, Plans and develop the project ideas, Monitors the progress of the project, Ensures Stakeholders satisfaction, Coordinates with the stakeholders, Manages documents and reports, Directs the team",
+      image:
+        "https://d1ka0itfguscri.cloudfront.net/zyIy/2024/01/30/07/03/cZVvieVJNKw/preview.jpg",
+      tags: [
+        "Project Management", "Technical Documentation", "Leading the Team", "Agile Methodology", "Trello",
+      ],
+      category: "web app"
+    },
+  ];
+  
+  export const TimeLineData = [
+    { year: 2017, text: " " },
+    { year: 2018, text: " " },
+    { year: 2019, text: " " },
+    { year: 2020, text: " " },
+    { year: 2021, text: " " },
+  ];
